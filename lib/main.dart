0185763b014425bfd2521c7858f6a96e3ce2f04d
Ronie de Jesus Nogueira <ronie.nogueira@outlook.com>
2021-12-12 11:23:44 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:buscador_de_gifs/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        hintColor: Colors.white,
+      ),
+    ),
+  );
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,158 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+import 'gif_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _search = '';
+  int _offset = 0;
+  final int _limit = 19;
+
+  Future _getGifs() async {
+    Response response;
+
+    if (_search == '') {
+      response = await Dio().get(
+          'https://api.giphy.com/v1/gifs/trending?api_key=3N4uwQFXSMKxkYFQL4BUIVcjrCDpiZoc&limit=25&rating=g');
+    } else {
+      response = await Dio().get(
+          'https://api.giphy.com/v1/gifs/search?api_key=3N4uwQFXSMKxkYFQL4BUIVcjrCDpiZoc&q=$_search&limit=$_limit&offset=$_offset&rating=g&lang=pt');
+    }
+
+    return response.data;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getGifs();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network(
+            'https://developers.giphy.com/branch/master/static/header-logo-0fec0225d189bc0eae27dac3e3770582.gif'),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Pesquise aqui',
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(),
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ),
+                textAlign: TextAlign.center,
+                onSubmitted: (text) {
+                  setState(() {
+                    _search = text;
+                    _offset = 0;
+                  });
+                }),
+          ),
+          Expanded(
+            child: FutureBuilder(
+                future: _getGifs(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return _createGifTable(context, snapshot);
+                  }
+
+                  return Container(
+                    alignment: Alignment.center,
+                    child: const CircularProgressIndicator(),
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+
+  int _getCount(List data) {
+    if (_search == '') {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0,),
+      itemCount: _getCount(snapshot.data['data']),
+      itemBuilder: (context, index) {
+        if (_search == '' || index < snapshot.data['data'].length) {
+          return GestureDetector(
+            child: FadeInImage.memoryNetwork(
+              placeholder: kTransparentImage,
+              image: snapshot
+                  .data['data'][index]['images']['fixed_height']['url'],
+              height: 300.0,
+              fit: BoxFit.cover,
+            ),
+            onTap: () {
+              Navigator.push(context,
+                MaterialPageRoute(builder: (context) =>
+                    GifPage(snapshot.data['data'][index])),
+              );
+            },
+            onLongPress: () {
+              Share.share(snapshot
+                  .data['data'][index]['images']['fixed_height']['url']);
+            },
+          );
+        } else {
+          return GestureDetector(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(
+                    Icons.add,
+                    color: Colors.white,
+                    size: 70.0,
+                  ),
+                  Text(
+                    'Carregar mais...',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22.0,
+                    ),
+                  ),
+                ],
+              ),
+              onTap: () {
+                setState(() {
+                  _offset += 19;
+                });
+              }
+          );
+        }
+      },
+    );
+  }
+}
